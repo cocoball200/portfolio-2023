4f84b393fb6581ab7868e667769a7891884dddba
cocoball200 <cocoball200@gmail.com>
2023-01-25 16:32:51 +0900

    add: title and description

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+  async rewrites() {
+    return [
+      {
+        destination: 'https://medium2.p.rapidapi.com/:path*',
+        source: '/article/:path*',
+      },
+    ]
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
